43ad671e35d071a3b44f83da6bc2d0e811fa107f
bouna <papabounabambabar@esp.sn>
2022-06-08 16:06:30 +0000

    initial Commit

---
+++ models/ECG_Metadata.model.js
@@ -0,0 +1,112 @@
+const mongoose = require('mongoose');
+
+const metadataSchema = new mongoose.Schema(
+    {
+        ECG: {
+           type: String,
+           unique: true,
+           require: true
+        },
+        metadata: {
+           type: String,
+            unique: true,
+            // require: true
+        },
+        recording: {
+            stared_at: {
+                type: Date,
+            },
+            end_at: {
+                type: Date,
+            },
+            sampling_rate: {
+                type: Number
+            },
+            filter: {
+                visit: {
+                    type: String,
+                    trim: true
+                },
+                duration: {
+                    type: String,
+                    trim: true
+                }
+            },
+        },
+        measurements: [{
+            name: {
+                type: String,
+                trim: true
+            },
+            values: {
+                type: String,
+                trim: true
+            },
+            description: {
+                type: String,
+                trim: true
+            },
+            lead: {
+                type: String,
+                trim: true
+            }
+        }],
+        patient: {
+            age: {
+                type: Number,
+            },
+            height: {
+                type: String,
+                trim: true
+            },
+            weight: {
+                type: String,
+                trim: true
+            },
+            sex: {
+                type: String,
+                trim: true
+            },
+            race: {
+                type: String,
+                trim: true
+            },
+            observation: {
+                type: String,
+                trim: true
+            }
+        },
+        annotations: [{
+            name: {
+                type: String,
+                trim: true
+            },
+            lead: {
+                type: String,
+                trim: true
+            },
+            data: {
+                type: String,
+                trim: true
+            },
+            description: {
+                type: String,
+                trim: true
+            }
+        }],
+        treatment: {
+            type: String,
+            trim: true
+        },
+        additional_information: {
+            type: String,
+            trim: true
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const ECGMetadataModel = mongoose.model("ECGMetadata", metadataSchema);
+module.exports = ECGMetadataModel;
---
+++ server.js
@@ -0,0 +1,37 @@
+//Env variable and db config
+require('dotenv').config({path: './config/.env'});
+require('./config/db');
+
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const app = express();
+
+//Some middleware..
+app.use(bodyParser.json());
+app.use(cors());
+
+
+//Routes
+const ECGRoutes = require('./routes/ECG.route');
+const dataSetRoutes = require('./routes/dataSet.route');
+
+app.use('/api/ecg', ECGRoutes);
+app.use('/api/dataset', dataSetRoutes);
+
+//app GET
+app.get('/', (req, res) => {
+    res.send('Welcome');
+})
+
+
+
+
+
+
+app.listen(process.env.PORT, (err) => {
+    if (!err)
+        console.log("Connected at : http://localhost:"+process.env.PORT);
+    else
+        console.log("No connected :", err);
+})
